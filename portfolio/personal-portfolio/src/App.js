f28b74d9e93269b5d02f91bc70f53667375e4493
Shivani <shivanirana0080@gmail.com>
2024-06-21 12:21:10 +0530

    change

--- portfolio/personal-portfolio/src/App.js
+++ portfolio/personal-portfolio/src/App.js
@@ -4,7 +4,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
+// import { Contact } from "./components/Contact";
 
 
 import { About }  from './components/About'
